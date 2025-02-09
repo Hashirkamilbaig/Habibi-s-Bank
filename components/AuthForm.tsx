c6b2ab767e9c6fffbbab9e1d6278d68f9ca7aa1e
hashirkamilbaig <hashirkamilbaig@gmail.com>
2025-02-09 07:24:24 -0800

    Made new fields for the sign up form updated how authFormSchema works in util, then updated the AuthForm.tsx and CustomInputs to adjust to the new authFormSchema by creating a new variable called formSchema and passing in the type of the page

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -28,8 +28,10 @@ const AuthForm = ({type}: {type : string}) => {
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
-  const form = useForm<z.infer<typeof authFormSchema>>({
-    resolver: zodResolver(authFormSchema),
+  const formSchema = authFormSchema(type);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
       password: ''
@@ -37,7 +39,7 @@ const AuthForm = ({type}: {type : string}) => {
   })
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof authFormSchema>) {
+  function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     setIsLoading(true)
@@ -78,6 +80,53 @@ const AuthForm = ({type}: {type : string}) => {
         <>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+
+              {type === 'sign-up' && (
+                <>
+                  <CustomInput 
+                  control={form.control}
+                  name='firstName'
+                  label="First Name"
+                  placeholder="Enter your first name"
+                  />
+                  <CustomInput 
+                  control={form.control}
+                  name='lastName'
+                  label="Last Name"
+                  placeholder="Enter your last name"
+                  />
+                  <CustomInput 
+                  control={form.control}
+                  name='address1'
+                  label="Address"
+                  placeholder="Enter your specific address"
+                  />
+                  <CustomInput 
+                  control={form.control}
+                  name='state'
+                  label="State"
+                  placeholder="Example: BC"
+                  />
+                  <CustomInput 
+                  control={form.control}
+                  name='postalCode'
+                  label="Postal Code"
+                  placeholder="Example: V3S 2S1"
+                  />
+                  <CustomInput 
+                  control={form.control}
+                  name='dateOfBirth'
+                  label="Date of Birth"
+                  placeholder="YYYY-MM-DD"
+                  />
+                  <CustomInput 
+                  control={form.control}
+                  name='ssn'
+                  label="SSN"
+                  placeholder="Example: 1234"
+                  />
+                </>
+              )}
               
               <CustomInput 
               control={form.control}
